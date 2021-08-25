98681b87624f5444dc46068af3625e81812800ec
Rigoberto Perez Ovando <rperez@cobach.edu.mx>
2021-08-24 19:49:25 -0500

    version 2
    Xml generado desde una BD

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "Hola generador XML";
+echo '<br>';
 CrearXML();
 
 function CrearXML(){
@@ -7,31 +8,37 @@ function CrearXML(){
     $documento->formatOutput=true;
     $raiz=$documento->createElement("ALUMNOS");
     $raiz=$documento->appendChild($raiz);
+    $mysqlConexion=new mysqli("localhost","root","","agencia");
+    $sqlConsulta="Select * from alumnos";
+    $resultado=$mysqlConexion->query($sqlConsulta);
+    while($row=$resultado->fetch_assoc())
+    {
+        $alumno=$documento->createElement("Alumno");
+        $alumno=$raiz->appendChild($alumno);
 
-    $alumno=$documento->createElement("Alumno");
-    $alumno=$raiz->appendChild($alumno);
+        $id=$documento->createElement("Id");
+        $id=$alumno->appendChild($id);
+        $textId=$documento->createTextNode($row["Id"]);
+        $textId=$id->appendChild($textId);
 
-    $id=$documento->createElement("Id");
-    $id=$alumno->appendChild($id);
-    $textId=$documento->createTextNode(1);
-    $textId=$id->appendChild($textId);
+        $nombre=$documento->createElement("Nombre");
+        $nombre=$alumno->appendChild($nombre);
+        $textNombre=$documento->createTextNode($row["Nombre"]);
+        $textNombre=$nombre->appendChild($textNombre);
 
-    $nombre=$documento->createElement("Nombre");
-    $nombre=$alumno->appendChild($nombre);
-    $textNombre=$documento->createTextNode("Bartolome");
-    $textNombre=$nombre->appendChild($textNombre);
+        $direccion=$documento->createElement("Direccion");
+        $direccion=$alumno->appendChild($direccion);
 
-    $direccion=$documento->createElement("Direccion");
-    $direccion=$alumno->appendChild($direccion);
+        $colonia=$documento->createElement("Colonia");
+        $colonia=$direccion->appendChild($colonia);
+        $textColonia=$documento->createTextNode($row["Colonia"]);
+        $textColonia=$colonia->appendChild($textColonia);
 
-    $calle=$documento->createElement("Calle");
-    $calle=$direccion->appendChild($calle);
-    $textCalle=$documento->createTextNode("Central");
-    $textCalle=$calle->appendChild($textCalle);
-    $colonia=$documento->createElement("Colonia");
-    $colonia=$direccion->appendChild($colonia);
-    $textColonia=$documento->createTextNode("Teran");
-    $textColonia=$colonia->appendChild($textColonia);
-    echo 'generar'. $documento->save("/Users/repto/Downloads/misalumnos.xml").'bytes <br>';
+        $numero=$documento->createElement("Numero");
+        $numero=$direccion->appendChild($numero);
+        $textNumero=$documento->createTextNode($row["Numero"]);
+        $textNumero=$numero->appendChild($textNumero);
+    }
+    echo 'Se generó archivo de '. $documento->save("/Users/repto/Downloads/misalumnos.xml").'bytes <br>';
 }
 ?>
